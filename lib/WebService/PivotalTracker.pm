4df2bedfdf9960052e6fb05b70685e2cb7df7cb5
Dave Rolsky <autarch@urth.org>
2016-05-26 15:24:08 -0500

    Add some very minimal docs

--- lib/WebService/PivotalTracker.pm
+++ lib/WebService/PivotalTracker.pm
@@ -117,3 +117,24 @@ sub _build_client {
 1;
 
 # ABSTRACT: Perl library for the Pivotal Tracker REST API
+
+__END__
+
+=head1 SYNOPSIS
+
+    my $pt =  WebService::PivotalTracker->new(
+        token => '...',
+    );
+    my $story = $pt->story( story_id => 1234 );
+    my $me = $pt->me;
+
+    for my $label ( $story->labels ) { ... }
+
+    for my $comment ( $story->comments ) { ... }
+
+=head1 DESCRIPTION
+
+B<This is very alpha (and as of yet mostly undocumented) software>.
+
+This module provides a Perl interface to the L<Pivotal
+Tracker|https://www.pivotaltracker.com/> REST API.
