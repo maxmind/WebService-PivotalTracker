72a9fde011bb2c34e88a96ca05c0e373cad9fcf4
Dave Rolsky <autarch@urth.org>
2016-06-14 15:31:46 -0500

    Bump version after release

--- lib/WebService/PivotalTracker/Entity.pm
+++ lib/WebService/PivotalTracker/Entity.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use DateTime::Format::RFC3339;
 use URI;
--- lib/WebService/PivotalTracker/Project.pm
+++ lib/WebService/PivotalTracker/Project.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use Params::CheckCompiler qw( compile );
 use WebService::PivotalTracker::PropertyAttributes;
